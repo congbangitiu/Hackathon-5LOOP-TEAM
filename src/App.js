d9a31b553d6af7a235ec98af6f3c2df3b0f922b9
Quang-Dieu Nguyen <86088186+itzmealvin@users.noreply.github.com>
2024-07-05 11:15:31 +0700

    style: refine App.js

--- src/App.js
+++ src/App.js
@@ -63,7 +63,6 @@ function App() {
         <Route path="/terms" element={<Terms />} />
         <Route path="/privacy" element={<Privacy />} />
         <Route path="/test" element={<Test />} />
-
         <Route path="*" element={<NotFound />} />
       </Routes>
 
